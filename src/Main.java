ea4e588d72c0231c8c753645b2bb4995173aa784
Laura Martinez Ortiz <lauramartinezo182@gmail.com>
2021-04-16 13:59:05 -0500

    Method organizeDisks fixed

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,80 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 
 
 public class Main {
-	
-	public int[] moves = new int[3];
-	
+
+	static BufferedReader objReader;
+	static PrintWriter objWriter;
+	public static final int EXAMPLE = 0;
+	public static final int HANOI = 1;
+
+	public static int n; 
+	public static int firstPivot = 1; 
+	public static int tempPivot = 2; 
+	public static int lastPivot = 3;
+	public static int[] moves = new int[3];
+
 	public static void main(String[] args) throws IOException {
+		//Escoja EXAMPLE para resolver el ejemplo y use HANOI para resolver los casos de prueba
+		int option = EXAMPLE;
+		String input = "";
+		String output = "";
+		switch(option) {
+		case EXAMPLE:
+			input = "C:\\Users\\prestamo\\Desktop\\eclipse-workspace\\hanoi-towers\\textFiles\\inputExample.txt";
+			output = "C:\\Users\\prestamo\\Desktop\\eclipse-workspace\\hanoi-towers\\textFiles\\outputExample.txt";
+			break;
+		case HANOI: 
+			input = "C:\\Users\\prestamo\\Desktop\\eclipse-workspace\\hanoi-towers\\textFiles\\hanoiInput.txt";
+			output = "C:\\Users\\prestamo\\Desktop\\eclipse-workspace\\hanoi-towers\\textFiles\\hanoiOutput.txt";
+			break;
+		default: 
+			System.out.println("Opción inválida");
+			return;
+		}
 
-		BufferedWriter objWriter = new BufferedWriter(new PrintWriter("Output"));
+		objReader = new BufferedReader(new FileReader(input));
+		objWriter = new PrintWriter(new PrintWriter(output));
 
-		BufferedReader objReader = new BufferedReader(new FileReader("Input"));
 		//m = total of problems
 		int m = Integer.parseInt(objReader.readLine());
+
+
 		//n = number of disks for each problem
 		for (int i=1;i<=m;i++) {
-			int n = Integer.parseInt(objReader.readLine());	
-			//organizeDisks(n, firstPivot, secondPivot, lastPivot);
+			int n = Integer.parseInt(objReader.readLine());
+			moves[0]= n;
+			System.out.println(moves[0]+ " "+moves[1]+ " "+moves[2]);
+			objWriter.println(moves[0]+ " "+moves[1]+ " "+moves[2]);
+			organizeDisks(n, firstPivot, lastPivot, tempPivot);
+			System.out.println(" ");
+			objWriter.println(" ");
+			moves= new int[3];
 		}
+		objWriter.close();
 		objReader.close();
 	}
-		
-}	
-	
-		
-	
-	
-public static void organizeDisks(int n, int firstPivot, int tempPivot, int lastPivot) throws IOException {
-	if(n == 1) {
 
-	}//imprimir
-	
-	 organizeDisks(n-1, firstPivot, tempPivot, lastPivot); 
-}
 
-public static void print() {
-	
-	
-}
+	public static void organizeDisks(int n, int firstPivot, int lastPivot, int tempPivot) throws IOException {
+		if(n == 1) {
+
+			moves[firstPivot-1]--;
+			moves[lastPivot-1]++;
+			System.out.println(moves[0]+ " "+moves[1]+ " "+moves[2]);
+			objWriter.println(moves[0]+ " "+moves[1]+ " "+moves[2]);
+			return;
+		}
+
+		organizeDisks(n-1, firstPivot, tempPivot, lastPivot);
+		moves[firstPivot-1]--;
+		moves[lastPivot-1]++;
+		objWriter.println(moves[0]+ " "+moves[1]+ " "+moves[2]);
+		System.out.println(moves[0]+ " "+moves[1]+ " "+moves[2]);
+		organizeDisks(n-1, tempPivot, lastPivot, firstPivot);
+	}
 }
